304ec31d5f59edda3341d39dc12163db78c93212
Trevor Jones <trevordavidjones@gmail.com>
2016-09-14 10:55:32 -0400

    add animation to favorite messages

--- app/assets/javascripts/controllers/framework_ctrl.js
+++ app/assets/javascripts/controllers/framework_ctrl.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module("app").controller("frameworkCtrl", function($scope, $http) {
+  angular.module("app").controller("frameworkCtrl", function($scope, $http, $timeout) {
     $scope.setup = function() {
       $http.get("/api/v1/frameworks.json").then(function(response) {
         $scope.frameworks = response.data;
@@ -10,9 +10,9 @@
 
     $scope.favorite = function(framework) {
       framework.favorite = framework.favorite ? 0 : 1
-      // rails prefers to pass numbers/strings in the params instead of booleans
+      // prefers to pass numbers/strings in the params instead of booleans
       // 0 is falsey in JS - so set favorite to 0 if 1, 1 if 0
-      // Then Rails can handle this appropriately
+      // Then Rails can handle the params appropriately
       var frameworkParams = {
         framework_id: framework.id,
         favorite: framework.favorite
@@ -20,6 +20,9 @@
       $http.post('/api/v1/favorites.json', frameworkParams).then(function(response) {
         framework.favoriteMessage = response.data.success;
       });
+      $timeout(function() {
+        framework.favoriteMessage = null;
+      }, 1500)
     }
   });
 })();
